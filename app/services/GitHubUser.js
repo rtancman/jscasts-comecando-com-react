37822d93ee1265fff6ccc33aeb6312f577608b1a
Raffael Tancman <raffael.tancman@m4u.com.br>
2016-11-11 12:07:31 -0200

    Template Strings

--- app/services/GitHubUser.js
+++ app/services/GitHubUser.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const GitHubUser = {
   getByUsername: function (username) {
-    return axios.get('https://api.github.com/users/' + username);
+    return axios.get(`https://api.github.com/users/${username}`);
   },
 
   getReposByUsername: function (username) {
-    return axios.get('https://api.github.com/users/' + username + '/repos');
+    return axios.get(`https://api.github.com/users/${username}/repos`);
   }
 };
 
