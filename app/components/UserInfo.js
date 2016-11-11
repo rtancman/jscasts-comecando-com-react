1c5f56fa20ba66ed2a45132564fb2848694a9c4c
Raffael Tancman <raffael.tancman@m4u.com.br>
2016-11-11 11:32:29 -0200

    Iterando e Renderizando com Loops

--- app/components/UserInfo.js
+++ app/components/UserInfo.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var UserRepos =  require('./UserRepos');
 
 function UserInfo(props) {
     var userInfo = props.user ?
@@ -10,6 +11,9 @@ function UserInfo(props) {
                     <p>{props.user.name}</p>
                     <p>Followers: {props.user.followers} / Following: {props.user.following}</p>
                     <p><a className="btn btn-default" href={props.user.html_url} role="details">View details</a></p>
+                </div>
+                <div className="col-lg-8">
+                    <UserRepos repos={props.repos}/>
                 </div>
             </div>
         ) : null;
--- public/bundle.js
+++ public/bundle.js
@@ -23032,42 +23032,48 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
+	var UserRepos = __webpack_require__(201);
 
 	function UserInfo(props) {
 	    var userInfo = props.user ? React.createElement(
-	        "div",
-	        { className: "row" },
+	        'div',
+	        { className: 'row' },
 	        React.createElement(
-	            "div",
-	            { className: "col-lg-4" },
-	            React.createElement("img", { className: "img-circle", src: props.user.avatar_url, alt: "avatar", width: "140px", height: "140px" }),
+	            'div',
+	            { className: 'col-lg-4' },
+	            React.createElement('img', { className: 'img-circle', src: props.user.avatar_url, alt: 'avatar', width: '140px', height: '140px' }),
 	            React.createElement(
-	                "h2",
+	                'h2',
 	                null,
 	                props.user.login
 	            ),
 	            React.createElement(
-	                "p",
+	                'p',
 	                null,
 	                props.user.name
 	            ),
 	            React.createElement(
-	                "p",
+	                'p',
 	                null,
-	                "Followers: ",
+	                'Followers: ',
 	                props.user.followers,
-	                " / Following: ",
+	                ' / Following: ',
 	                props.user.following
 	            ),
 	            React.createElement(
-	                "p",
+	                'p',
 	                null,
 	                React.createElement(
-	                    "a",
-	                    { className: "btn btn-default", href: props.user.html_url, role: "details" },
-	                    "View details"
+	                    'a',
+	                    { className: 'btn btn-default', href: props.user.html_url, role: 'details' },
+	                    'View details'
 	                )
 	            )
+	        ),
+	        React.createElement(
+	            'div',
+	            { className: 'col-lg-8' },
+	            React.createElement(UserRepos, { repos: props.repos })
 	        )
 	    ) : null;
 	    return userInfo;
@@ -23079,6 +23085,68 @@
 	};
 
 	module.exports = UserInfo;
+
+/***/ },
+/* 201 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var React = __webpack_require__(1);
+
+	var UserRepos = React.createClass({
+	    displayName: "UserRepos",
+
+	    render: function () {
+	        var repos = this.props.repos.map(function (repo, key) {
+	            return React.createElement(
+	                "div",
+	                { key: key, className: "thumbnail" },
+	                React.createElement(
+	                    "div",
+	                    { className: "caption" },
+	                    React.createElement(
+	                        "h3",
+	                        null,
+	                        repo.name,
+	                        " ",
+	                        React.createElement(
+	                            "span",
+	                            { className: "badge" },
+	                            repo.stargazers_count,
+	                            " STARS"
+	                        )
+	                    ),
+	                    React.createElement(
+	                        "p",
+	                        null,
+	                        repo.description
+	                    ),
+	                    React.createElement(
+	                        "p",
+	                        null,
+	                        React.createElement(
+	                            "a",
+	                            { href: repo.html_url, className: "btn btn-primary", role: "button" },
+	                            "Repository"
+	                        ),
+	                        React.createElement(
+	                            "a",
+	                            { href: repo.html_url + '/issues', className: "btn btn-default", role: "button" },
+	                            "Issues"
+	                        )
+	                    )
+	                )
+	            );
+	        });
+
+	        return React.createElement(
+	            "div",
+	            null,
+	            repos
+	        );
+	    }
+	});
+
+	module.exports = UserRepos;
 
 /***/ }
 /******/ ]);
